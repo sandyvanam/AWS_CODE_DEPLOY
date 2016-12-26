703dbfd97ba749a4993c77a730fda8167b02e850
sandyvanam <sandyvanam@gmail.com>
2016-12-26 18:06:06 +0530

    Update startapache.sh

--- scripts/startapache.sh
+++ scripts/startapache.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#service httpd start > /var/log/startapache.out 2>&1
-service apache2 start > /var/log/startapache.out 2>&1
+service httpd start > /var/log/startapache.out 2>&1
+#service apache2 start > /var/log/startapache.out 2>&1
