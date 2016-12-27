7924646df4f1a9017cbda296aecd18ee9ebc3f03
sandyvanam <sandyvanam@gmail.com>
2016-12-27 18:22:54 +0530

    Update installapache.sh

--- scripts/installapache.sh
+++ scripts/installapache.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #yum -y install httpd > /var/log/installapache.out 2>&1
-sudo apt-get install apache2 > /var/log/installapache.out 2>&1
+apt-get install apache2 > /var/log/installapache.out 2>&1
