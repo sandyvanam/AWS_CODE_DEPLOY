e9d377bbb321bc8715b8b597f206b76a16743868
sandyvanam <sandyvanam@gmail.com>
2019-11-05 12:26:21 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+<html>
+<head>
+<title>TAG index</title>
+</head>
+<body  bgcolor="blue"text="#000000" link="#0000ff" vlink="#800080" alink="#ff0000">
+<h1>
+<?php
+
+echo " Hellow World!!.. Thanks for using AWS Code Deploy ";
+
+?></h1>
+</body>
+</html>
+?>
